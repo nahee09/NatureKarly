95fd223079fc5ce1e54c8cb9fe1689726a919c8b
hyunwlee-dev <hwlee3928@gmail.com>
2023-03-10 15:29:11 +0900

    :wrench: [Config] add storybook config

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import './index.css';
-import './styles/global.css';
+import '@/styles/normalize.css';
+import '@/styles/reset.css';
+import '@/styles/global.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
@@ -8,7 +9,7 @@ import { RecoilLogger } from 'recoil-devtools-logger';
 
 // import { HelmetProvider } from 'react-helmet-async';
 // import { TimeTravelObserver } from './@recoilDebug';
-import App from './App';
+import App from '@/app/App';
 
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
