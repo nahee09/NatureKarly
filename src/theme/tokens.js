d5a7d178dda854ebae21da46f4b5a0af52068ecc
semin99 <jsm7494@naver.com>
2023-03-12 16:42:38 +0900

    :lipstick: [Style] tokens.js 추가#7

---
+++ src/theme/tokens.js
@@ -0,0 +1,54 @@
+export const tokens = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    secondary: '#90C090',
+    primary: '#509450',
+    content: '#333333',
+    gray: {
+      50: '#F9F9F9',
+      100: '#E1E1E1',
+      200: '#C4C4C4',
+      300: '#A6A6A6',
+      400: '#898989',
+      500: '#6B6B6B',
+      600: '#565656',
+      700: '#404040',
+      800: '#2B2B2B',
+      900: '#151515',
+    },
+    accent: '#FA622F',
+    blue: '#0066FF',
+    error: '#F03F40',
+  },
+  fontSize: {
+    sm: '0.75rem', //12px
+    md: '1rem', //16px
+    lg: '1.33125rem', //21.3px
+    xl: '1.775rem', //28.4px
+    xxl: '2.36875rem', //37.9px
+    xxxl: '3.15625rem', //50.5px
+  },
+  lineHeight: {
+    sm: '140%',
+    md: '150%',
+    lg: '160%',
+  },
+  boxShadow: {
+    above: {
+      'shadow/high': '0px -16px 48px rgba(0, 0, 0, 0.3)',
+      'shadow/medium': '0px -8px 36px rgba(0, 0, 0, 0.2)',
+      'shadow/low': '0px -4px 24px rgba(0, 0, 0, 0.1)',
+    },
+    below: {
+      'shadow/high': '0px 16px 48px rgba(0, 0, 0, 0.3)',
+      'shadow/medium': '0px 8px 36px rgba(0, 0, 0, 0.2)',
+      'shadow/low': '0px 4px 24px rgba(0, 0, 0, 0.1)',
+    },
+  },
+  borderRadius: {
+    none: '0',
+    default: '0.25rem',
+    md: '1rem',
+  },
+};
