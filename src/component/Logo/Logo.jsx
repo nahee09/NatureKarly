1194464ee11f768f74bcedb3e5b13ec4acdabe35
BOARAVORAYOON <yoonbora1803@naver.com>
2023-03-14 11:45:47 +0900

    ✨ [feature] 로고 생성

---
+++ src/component/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/logo.svg';
+
+const Logo = function () {
+  return (
+    <figure>
+      <img alt="마켓칼리네이쳐 로고 이미지" src={logo} />
+    </figure>
+  );
+};
+
+export default Logo;
