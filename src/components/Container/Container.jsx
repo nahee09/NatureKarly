e7893a1d8ed5513d27da968d100c3e94a6dae5f5
nahee09 <tink159976@gmail.com>
2023-03-14 17:14:06 +0900

    ✨[Feature] Container 컴포넌트 디자인

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,36 @@
+import { string, node, oneOf } from 'prop-types';
+
+import classes from './Container.module.css';
+
+export function Container({
+  as: Component,
+  className,
+  children,
+  align,
+  width,
+  ...restProps
+}) {
+  const combineClassNames =
+    `${classes[align]} ${classes[width]} ${className}`.trim();
+
+  return (
+    <Component className={combineClassNames} {...restProps}>
+      {children}
+    </Component>
+  );
+}
+
+Container.defaultProps = {
+  as: 'div',
+  className: '',
+  align: 'center',
+  width: 'widthSmall',
+};
+
+Container.propTypes = {
+  as: string,
+  className: string,
+  children: node,
+  align: oneOf(['center', 'left', 'right']),
+  width: oneOf(['widthSmall', 'widthMedium', 'widthLarge']),
+};
