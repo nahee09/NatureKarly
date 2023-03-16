67ec653361673b0b8a0ebe5d5e27971513be4026
hyunwlee-dev <hwlee3928@gmail.com>
2023-03-16 14:49:49 +0900

    :sparkles: [Feature] 디자인 icon component

--- src/components/IconComponent/IconComponent.jsx
+++ src/components/IconComponent/IconComponent.jsx
@@ -1,15 +1,41 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import classes from './IconComponent.module.css';
 
-export function IconComponent({ children, ...restProps }) {
+export function IconComponent({ children, dataProps, ...restProps }) {
+  const defaultOptions = {
+    width: '30px',
+    height: '30px',
+    backgroundPositionX: 'center',
+    backgroundPositionY: 'center',
+  };
+  const { width, height, backgroundPositionX, backgroundPositionY } = {
+    ...defaultOptions,
+    ...dataProps,
+  };
+
   return (
-    <div className={classes.IconComponent} {...restProps}>
+    <div
+      className={classes.IconComponent}
+      style={{
+        width,
+        height,
+        backgroundPositionX,
+        backgroundPositionY,
+      }}
+      {...restProps}
+    >
       {children}
     </div>
   );
 }
 
-// IconComponent.defaultProps = {};
-
-// IconComponent.propTypes = {};
+IconComponent.propTypes = {
+  children: PropTypes.node,
+  dataProps: PropTypes.shape({
+    width: PropTypes.string,
+    height: PropTypes.string,
+    backgroundPositionX: PropTypes.string,
+    backgroundPositionY: PropTypes.string,
+  }),
+};
