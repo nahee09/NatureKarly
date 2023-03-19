b894b874f889825c0d624747f0525e2e86e40ce9
nahee09 <tink159976@gmail.com>
2023-03-20 01:50:13 +0900

    ✨[Feature] EmptyContent 컴포넌트 디자인

---
+++ src/components/EmptyContent/EmptyContent.stories.jsx
@@ -0,0 +1,14 @@
+import { EmptyContent } from './EmptyContent';
+
+export default {
+  title: 'Components/EmptyContent',
+  component: EmptyContent,
+  tags: ['autodocs'],
+  args: { ...EmptyContent.defaultProps },
+};
+
+/* -------------------------------------------------------------------------- */
+
+export const Default = {
+  args: {},
+};
