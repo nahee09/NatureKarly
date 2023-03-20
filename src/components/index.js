dfe08291aa57c8d69c8fb46b4813258d59dd5bcf
nahee09 <tink159976@gmail.com>
2023-03-20 09:19:36 +0900

    [Chore] index.js에 컴포넌트 추가

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export * from './Product/Product';
 export * from './Button/Button';
 export * from './Input/Input';
 export * from './Label/Label';
+export * from './InternalLinkMove/InternalLinkMove';
+export * from './InternalLinkMoves/InternalLinkMoves';
