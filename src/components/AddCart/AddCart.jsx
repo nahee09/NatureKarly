769cb6aa1c3e8b7951e468382574f274182b89a1
BOARAVORAYOON <yoonbora1803@naver.com>
2023-03-21 10:44:10 +0900

    ✨ [Feature] 장바구니 담기 모달창 마크업 및 스타일링

--- src/components/AddCart/AddCart.jsx
+++ src/components/AddCart/AddCart.jsx
@@ -1,4 +1,4 @@
-import { A11yHidden } from '..';
+import { A11yHidden } from '../';
 
 import classes from './AddCart.module.css';
 
@@ -7,21 +7,18 @@ const AddCart = function ({ name, price, totalprice }) {
     <div className={classes.AddCart}>
       <h3>{name}</h3>
       <div className={classes.Price}>
-        <p>
-          {price}
-          <span>원</span>
-        </p>
+        <p>{price}원</p>
         <A11yHidden>
           <label htmlFor="a">수량카운터</label>
         </A11yHidden>
         <input id="a" name="" type="number" />
       </div>
-      <div className={classes.TotalPrice}>
+      <div className={classes.TotalPriceTable}>
         <p>합계</p>
-        <p>{totalprice}</p>
+        <p className={classes.TotalPrice}>{totalprice}원</p>
       </div>
-      <p>구매 시 5원 적립</p>
-      <div className="AddCartButton">
+      <p className={classes.PointGuide}>구매 시 5원 적립</p>
+      <div className={classes.AddCartButton}>
         <button>취소</button>
         <button>장바구니 담기</button>
       </div>
