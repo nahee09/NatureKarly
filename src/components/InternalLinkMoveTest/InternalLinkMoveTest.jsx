2f4378f87d0d3f8722a376260b16feb862adcfcd
nahee09 <tink159976@gmail.com>
2023-03-21 17:12:43 +0900

    🧪[Test] InternalLinkMoveTest 컴포넌트 추가

---
+++ src/components/InternalLinkMoveTest/InternalLinkMoveTest.jsx
@@ -0,0 +1,24 @@
+import {} from 'prop-types';
+
+import { InternalLinkMoves, Container } from '..';
+
+import classes from './InternalLinkMoveTest.module.css';
+import saladInfoImage from '@/assets/salad/detail_info.jpg';
+import saladViewImage from '@/assets/salad/detail_view.jpg';
+
+export function InternalLinkMoveTest() {
+  const internalLinkList = [
+    { id: 'item1', name: '상품설명', to: '#productDescription' },
+    { id: 'item2', name: '상세정보', to: '#productDetail' },
+    { id: 'item3', name: '후기', to: '#productReview', isNumberOption: true },
+    { id: 'item4', name: '문의', to: '#productInquiry' },
+  ];
+
+  return (
+    <Container className={classes.align}>
+      <InternalLinkMoves list={internalLinkList} title="상세 메뉴 리스트" />
+      <img alt="상품설명이미지" id="productDescription" src={saladViewImage} />
+      <img alt="상품상세정보이미지" id="productDetail" src={saladInfoImage} />
+    </Container>
+  );
+}
