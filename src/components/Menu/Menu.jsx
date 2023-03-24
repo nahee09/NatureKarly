2229c60eefa5f77b9c134623127fd1f4b0bc24e2
baeseongjae <3574214@naver.com>
2023-03-24 17:05:56 +0900

    ✨ [Feature] 메뉴 마우스 오버시 메뉴리스트 노출 기능 완성

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -11,25 +11,29 @@ export function Menu(props) {
   }
 
   function handleMouseLeave(e) {
+    if (
+      !e.relatedTarget ||
+      (e.target.className !== 'menuList' && e.relatedTarget.className !== 'menuList')
+    ) {
       setDropDown(false);
     }
   }
 
-  return(
-    <>
-      <button className={classes.menu} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+  return (
+    
+    <div className={classes.menuWrapper} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+      <button className={classes.menu}>
         카테고리
       </button>
       {dropDown && (
-          <ul className={`${classes.menuList}`}>
-            {menuList.map((item) => (
-                <li key={item.id}>
-                  <a href="#">{item.text}</a>
-                </li>
-              ))}
-          </ul>
+        <ul className={`${classes.menuList}`}>
+          {menuList.map((item) => (
+              <li key={item.id}>
+                <a href="#">{item.text}</a>
+              </li>
+            ))}
+        </ul>
       )}
-      
-    </>
+    </div>
   );
 }
