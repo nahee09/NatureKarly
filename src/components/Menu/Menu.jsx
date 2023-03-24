5690dc90973e97f3c389a04dd8215098e6f1d69b
baeseongjae <3574214@naver.com>
2023-03-24 14:40:51 +0900

    ✨ [Feature] 메뉴 리스트 마크업

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,20 @@
+
+
 import classes from './Menu.module.css';
 
-export function Menu() {
+export function Menu(props) {
+  const menuList = props.menuList;
+  
   return(
-    <button className={classes.menu}>
-      카테고리
-    </button>
+    <>
+      <button className={classes.menu}>
+        카테고리
+      </button>
+      <ul className={`${classes.menuList} ${classes.hidden}`}>
+        {menuList.map((item) => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+      </ul>
+    </>
   );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import PropTypes, { arrayOf, oneOf, string, shape } from 'prop-types';
 
 import { Menu } from '../Menu/Menu';
@@ -12,12 +14,37 @@ const NavigationItemType = shape({
   text: string,
 });
 
+const menuList = [
+  { id: 'gift', text: '선물하기' },
+  { id: 'vege', text: '채소' },
+  { id: 'fruit', text: '과일·견과·쌀' },
+  { id: 'fish', text: '수산·해산·건어물' },
+  { id: 'egg', text: '정육·계란' },
+  { id: 'main', text: '국·반찬·메인요리' },
+  { id: 'salad', text: '샐러드·간편식' },
+  { id: 'noodle', text: '면·양념·오일' },
+  { id: 'bever', text: '생수·음료·우유·커피' },
+  { id: 'snack', text: '간식·과자·떡' },
+  { id: 'bakery', text: '베이커리·치즈·델리' },
+  { id: 'health', text: '건강식품' },
+  { id: 'wine', text: '와인' },
+  { id: 'liquor', text: '전통주' },
+  { id: 'living', text: '생활용품·리빙·캠핑' },
+  { id: 'skin', text: '스킨케어·메이크업' },
+  { id: 'hair', text: '헤어·바디·구강' },
+  { id: 'kitchen', text: '주방용품' },
+  { id: 'home', text: '가전제품' },
+  { id: 'animal', text: '반려동물' },
+  { id: 'baby', text: '베이비·키즈·완구' },
+  { id: 'trip', text: '여행·티켓' },
+];
+
 export function Navigation({ as, headline, list, ...restProps }) {
   return (
     <>
       <A11yHidden as={as}>{headline}</A11yHidden>
       <nav className={classes.nav} {...restProps}>
-        <Menu />
+        <Menu menuList={menuList} />
         <ul className={classes.navMenu}>
           {list.map((item) => (
             <li key={item.id}>
