459962d2ecb194e45d016381b6206b303ba0ea9c
baeseongjae <3574214@naver.com>
2023-03-24 16:18:15 +0900

    ✨ [Feature] 헤더 메뉴바 마우스 오버시 메뉴리스트 출력

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,20 +1,35 @@
-
+import React, { useState } from 'react';
 
 import classes from './Menu.module.css';
 
 export function Menu(props) {
   const menuList = props.menuList;
-  
+  const [dropDown, setDropDown] = useState(false);
+
+  function handleMouseOver() {
+    setDropDown(true);
+  }
+
+  function handleMouseLeave(e) {
+      setDropDown(false);
+    }
+  }
+
   return(
     <>
-      <button className={classes.menu}>
+      <button className={classes.menu} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
         카테고리
       </button>
-      <ul className={`${classes.menuList} ${classes.hidden}`}>
-        {menuList.map((item) => (
-            <li key={item.id}>{item.text}</li>
-          ))}
-      </ul>
+      {dropDown && (
+          <ul className={`${classes.menuList}`}>
+            {menuList.map((item) => (
+                <li key={item.id}>
+                  <a href="#">{item.text}</a>
+                </li>
+              ))}
+          </ul>
+      )}
+      
     </>
   );
 }
