a6bbe00b19e2c178f7863b7cb3b0db2518a93c8e
baeseongjae <3574214@naver.com>
2023-03-26 18:07:50 +0900

    ✨ [Feature] 카테고리 포커스시 메뉴 노출 기능 아이콘 삽입

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 
 import classes from './Menu.module.css';
 
+import { IconComponent } from '@/components';
+
 export function Menu(props) {
   const menuList = props.menuList;
+  
   const [dropDown, setDropDown] = useState(false);
-
+    
   function handleMouseOver() {
     setDropDown(true);
   }
@@ -20,7 +23,6 @@ export function Menu(props) {
   }
 
   return (
-    
     <div className={classes.menuWrapper} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
       <button className={classes.menu}>
         카테고리
@@ -28,7 +30,18 @@ export function Menu(props) {
       {dropDown && (
         <ul className={`${classes.menuList}`}>
           {menuList.map((item) => (
-              <li key={item.id}>
+              <li key={item.id} className={classes.menuItem}>
+                <IconComponent 
+                  data-image={item.id} 
+                  style={{ width: '24px', 
+                          height: '24px', 
+                          backgroundSize : '24px 24px', 
+                          backgroundPosition: '50% 50%',
+                          position: 'absolute',
+                          top: '8px',
+                          left: '8px',
+                        }}>
+                </IconComponent>
                 <a href="#">{item.text}</a>
               </li>
             ))}
