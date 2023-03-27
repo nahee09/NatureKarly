491692dda5b477dff40084864ca9de5b329dbbdb
semin99 <jsm7494@naver.com>
2023-03-27 17:30:25 +0900

    [Chore] 데이터베이스 키 수정

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,12 +1,24 @@
 import { initializeApp } from 'firebase/app';
 
+const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_MESSAGE_SENDER_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_APP_ID,
+} = import.meta.env;
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyAnhLCbF8MOOTpVeeYCNbDac0JKAr2R4-o',
-  authDomain: 'naturekarly-68027.firebaseapp.com',
-  projectId: 'naturekarly-68027',
-  storageBucket: 'naturekarly-68027.appspot.com',
-  messagingSenderId: '401010153733',
-  appId: '1:401010153733:web:0d96a2e4e012091255ff6c',
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_MESSAGE_SENDER_ID,
+  messagingSenderId: VITE_STORAGE_BUCKET,
+  appId: VITE_APP_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
