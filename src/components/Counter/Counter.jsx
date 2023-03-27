f835666b9c6f54aba552a8877c543ec54ed311ef
BOARAVORAYOON <yoonbora1803@naver.com>
2023-03-27 11:36:32 +0900

    ✨ [Feature] counter 버튼을 클릭함에 따른 수량 증가 감소 기능 추가

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,15 +2,26 @@ import classes from './Counter.module.css';
 import { A11yHidden } from '@/components/A11yHidden/A11yHidden';
 import { Button } from '@/components/Button/Button';
 import { Input } from '@/components/Input/Input';
+import { useState } from 'react';
 
 const Counter = function () {
+  const [count, setCount] = useState(1);
+
+  const onDecrease = function () {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  const onIncrease = function () {
+    setCount(prevCount => prevCount + 1)
+  }
+
   return (
     <div className={classes.Counter}>
-      <Button className={classes.MinusButton}>
+      <Button className={count == 0 ? classes.DisabledMinusButton : classes.MinusButton} onClick={onDecrease} disabled={count == 0 ? true : false}>
         <A11yHidden>수량감소</A11yHidden>
       </Button>
-      <Input className={classes.CountInput} min="0" type={'number'} value="1" />
-      <Button className={classes.PlusButton}>
+      <Input className={classes.CountInput} min={0} type={'number'} value={count} />
+      <Button className={classes.PlusButton} onClick={onIncrease}>
         <A11yHidden>수량증가</A11yHidden>
       </Button>
     </div>
