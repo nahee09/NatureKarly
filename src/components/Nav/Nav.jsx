b55bf4e7408b95fc0c034e53f311e2f888b7a829
baeseongjae <3574214@naver.com>
2023-03-27 15:32:34 +0900

    💄 [Style] Nav 및 NavSide 컴포넌트 스타일 변경

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -52,10 +52,6 @@ export function Navigation({ as, headline, list, ...restProps }) {
             </li>
           ))}
         </ul>
-        <a href='#' className={classes.navSide}>
-          <span>샛별·낮</span>
-          <span> 배송안내</span>
-        </a>
       </nav>
     </>
   );
