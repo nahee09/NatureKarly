a73135cd24223e4b35d775cab971d06e800ef245
nahee09 <tink159976@gmail.com>
2023-03-27 17:00:27 +0900

    ♻️[Refactor] 모달창 켜지면 제목 input에 초점이 가도록 함

--- src/components/ProductPopUpInput/ProductPopUpInput.jsx
+++ src/components/ProductPopUpInput/ProductPopUpInput.jsx
@@ -1,16 +1,36 @@
 import {} from 'prop-types';
+import { useEffect, useRef } from 'react';
+
+import { usesetRecoilState, useRecoilValue } from 'recoil';
 
 import classes from './ProductPopUpInput.module.css';
+import { showModalState } from '@/@store/modalStates';
+import { titleText } from '@/@store/popUpTextState';
 
 export function ProductPopUpInput({ inputName }) {
+  const setTitle = usesetRecoilState(titleText);
+  const titleInput = useRef();
+  const modal = useRecoilValue(showModalState);
+  function getTitleText(e) {
+    setTitle(e.target.value);
+  }
+
+  useEffect(() => {
+    if (modal) {
+      titleInput.current.focus();
+    }
+  }, [modal]);
+
   return (
     <input
+      ref={titleInput}
       className={classes.formInput}
       id={inputName}
       maxLength="35"
       name={inputName}
       placeholder="제목을 입력해주세요"
       type="text"
+      onChange={getTitleText}
     />
   );
 }
