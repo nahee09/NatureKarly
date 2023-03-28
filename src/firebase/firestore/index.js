1da3d6ee383d2fca1d5f87f6b2faa09cadd9b4fd
hyunwlee-dev <hwlee3928@gmail.com>
2023-03-28 16:26:21 +0900

    :rocket: [Deploy] nature market karly project

--- src/firebase/firestore/index.js
+++ src/firebase/firestore/index.js
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 export const db = getFirestore(firebaseApp);
 
 export * from './useCreateAuthUser';
-export * from './useWriteBatchData';
+export * from './useWriteBatchData'; 
 
 export * from './useCreateData';
 export * from './useReadData';
--- vite.config.js
+++ vite.config.js
@@ -19,4 +19,5 @@ export default defineConfig({
   define: {
     'process.env': {},
   },
+  base: '/'
 });
